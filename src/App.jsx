dade6b02999b8ecf80e39a204cbcecc6df6e0f0f
haha861924 <qq861924@gmail.com>
2022-01-23 01:19:43 +0800

    feat: 🎸 search bar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+
+function App() {
+  return (
+    <div className='ui container' style={{ marginTop: 20 }}>
+      <SearchBar />
+    </div>
+  )
+}
+
+export default App
