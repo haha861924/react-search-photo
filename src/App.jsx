ee5c58542ac5f9fcb8b6d91515546c4e1bfad7e7
haha861924 <qq861924@gmail.com>
2022-02-01 18:35:01 +0800

    feat: 🎸 search keywords to show api images

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 // App.jsx
-import React, { useState } from 'react'
-import SearchBar from './components/SearchBar'
-import ImageList from './components/ImageList'
-import unsplash from './api/unsplash'
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import unsplash from './api/unsplash';
+import './App.css'
 
 function App() {
-  const [inputState, setInputState] = useState({ text: '' })
-  const [imgList, setImgList] = useState({ images: [] })
+  const [inputState, setInputState] = useState({ text: '' });
+  const [imgList, setImgList] = useState({ images: [] });
 
   const fetchSearchPhoto = async (input) => {
-    const res = await unsplash.get('/search/photos', { params: { query: input } })
-    setImgList({ img: res.data.results })
-    console.log('🚀 ~ fetchSearchPhoto ~ img', res.data.results)
-  }
+    const res = await unsplash.get('/search/photos', { params: { query: input, page: 2 } });
+    setImgList({ images: res.data.results });
+  };
 
   const onSearchSubmit = (input) => {
-    console.log(input)
-    fetchSearchPhoto(input)
-    setInputState({ text: input })
-    console.log('setState', inputState)
-  }
+    console.log(input);
+    fetchSearchPhoto(input);
+    setInputState({ text: input });
+    console.log('setState', inputState);
+  };
 
   return (
     <div className="ui container top20">
       <SearchBar handleSubmit={onSearchSubmit} />
       <ImageList images={imgList.images} />
-      <div className="ui message yellow">{inputState.text}</div>
-      <div className="ui message red">{imgList.img}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
